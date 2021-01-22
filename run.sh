2542f2c05292d5bee0fbd0232deb4761cd4feded
Gilles Freart <gilfre5@orion.home.local>
2021-01-22 10:38:26 +0100

    Adding optionnal cluster name argument to the run.sh

--- run.sh
+++ run.sh
@@ -8,9 +8,45 @@
 #                                                                                            #
 # ------------------------------------------------------------------------------------------ #
 
+SCRIPT_NAME=`basename "$0"`
+EXTRA_VARS=
+
+usage ()
+{
+  echo
+  echo "usage : ${SCRIPT_NAME} [ -c cluster_name ] "
+  echo
+  echo " f.i. : ${SCRIPT_NAME} -c \"c7x-postgresql-ha\""
+  echo
+
+  exit 1
+}
+
+while getopts ":c:" opt; do
+  case ${opt} in
+    c)
+	EXTRA_VARS="${EXTRA_VAR}cluster_name='${OPTARG}' "
+        ;;
+
+    \?)
+        usage
+        ;;
+
+    :)
+        usage
+        ;;
+  esac
+done
+
 if [ ! -d ~/.ansible/collections/ansible_collections/community/libvirt ];
 then
   ansible-galaxy collection install -r collections/requirements.yaml
 fi
 
-ansible-playbook main.yaml
+if [ ! -z "${EXTRA_VARS}" ]
+then
+  ansible-playbook main.yaml --extra-var "${EXTRA_VARS}"
+else  
+  ansible-playbook main.yaml
+fi	
+
